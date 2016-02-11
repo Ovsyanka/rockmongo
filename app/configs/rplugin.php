6fc6e66d6899bd372804293b2ac5ff8681afffff
or <ideyashkin@avinfors.ru>
2016-02-11 17:09:11 +0300

    Добавил список плагинов в конфиг. Включил ace.

--- app/configs/rplugin.php
+++ app/configs/rplugin.php
@@ -2,4 +2,10 @@
 
 //$plugins["PLUGIN_NAME"]["enabled"] = 1;
 
+$plugins["ace"]["enabled"] = 1;
+$plugins['gridfs']['enabled'] = 0;
+$plugins['mapreduce']['enabled'] = 0;
+$plugins['sampleplugin']['enabled'] = 0;
+$plugins['sharding']['enabled'] = 0;
+$plugins['systemjs']['enabled'] = 0;
 ?>
